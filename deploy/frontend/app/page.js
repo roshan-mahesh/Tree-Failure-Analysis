18fd6ef4ecbd15d79319fea89566694fac849fa8
Notebooked <creepinglion09@gmail.com>
2025-09-20 17:11:21 -0700

    PUSH FETCH RENDER

--- deploy/frontend/app/page.js
+++ deploy/frontend/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/evaluate_tree", {
+      const response = await fetch("https://tree-failure-analysis.onrender.com/api/evaluate_tree", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
